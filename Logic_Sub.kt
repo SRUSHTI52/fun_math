89d60216f832dd778e1f1f33d9d9b7db493b874d
SRUSHTI PORIWADE <srushtiporiwade02@gmail.com>
2024-04-14 11:02:47 +0530

    Update Logic_Sub.kt

--- Logic_Sub.kt
+++ Logic_Sub.kt
@@ -107,7 +107,7 @@ start()
         val n1 = Random.nextInt(0, 100)
         val n2 = Random.nextInt(0, 100)
         textque.text = "$n1 - $n2"
-        ans = n1 + n2
+        ans = n1 - n2
         timecounter()
     }
 
